d55422e43a4be44100995a78f79842ebb3b8fcb0
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-02 22:19:46 +0200

    Update deps

--- package-deb.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -e
-poetry install
-poetry build
-PKG_NAME=$(poetry version | awk '{print $1}' | sed 's/-/_/g')
-PKG_VERSION=$(poetry version | awk '{print $2}')
-wheel2deb -v --search-path ./dist
