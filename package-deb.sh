1f57d06ccd9dbb6550001c7d11f119563d5e7d45
Philipp Hossner <philipp.hossner@posteo.de>
2024-04-02 21:35:11 +0200

    Update deps

--- package-deb.sh
+++ package-deb.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 poetry install
-poetry build -f sdist
+poetry build
 PKG_NAME=$(poetry version | awk '{print $1}' | sed 's/-/_/g')
 PKG_VERSION=$(poetry version | awk '{print $2}')
 wheel2deb
